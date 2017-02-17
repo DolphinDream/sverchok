e238d37ffd3706897de6c6541230292b8c687577
Marius Giurgi <marius.giurgi@gmail.com>
2017-02-17 14:39:22 -0500

    more custom icon updates

--- ui/sv_icons.py
+++ ui/sv_icons.py
@@ -3,26 +3,27 @@ import os
 import glob
 import bpy.utils.previews
 
-import sverchok
+DEBUG = True
+def logDebug(message, extra=""):
+    if DEBUG:
+        print(message, extra)
 
-# custom icons dictionary
-_icon_collection = {}
-addon_name = sverchok.__name__
+# global variable to store icons in
+custom_icons = None
 
-def custom_icon(name):
-    load_custom_icons()  # load in case they custom icons not already loaded
+def customIcon(name):
+    logDebug("customIcon called: ", name)
+    global custom_icons
 
-    custom_icons = _icon_collection["main"]
+    if name in custom_icons:
+        return custom_icons[name].icon_id
+    else:
+        logDebug("No custom icon found for name: ", name)
+        return 0
 
-    default = lambda: None  # for no icon with given name will return zero
-    default.icon_id = 0
-
-    return custom_icons.get(name, default).icon_id
-
-
-def load_custom_icons():
-    if len(_icon_collection):  # return if custom icons already loaded
-        return
+def loadCustomIcons():
+    logDebug("loadIcons called")
+    global custom_icons
 
     custom_icons = bpy.utils.previews.new()
 
@@ -30,59 +31,29 @@ def load_custom_icons():
     iconPattern = "sv_*.png"
     iconPath = os.path.join(iconsDir, iconPattern)
     iconFiles = [os.path.basename(x) for x in glob.glob(iconPath)]
+    logDebug(iconFiles)
 
+    iconIDs=[]
     for iconFile in iconFiles:
         iconName = os.path.splitext(iconFile)[0]
         iconID = iconName.upper()
+        iconIDs.append(iconID)
+        logDebug(iconID)
         custom_icons.load(iconID, os.path.join(iconsDir, iconFile), "IMAGE")
 
-    _icon_collection["main"] = custom_icons
-
-
-def remove_custom_icons():
-    for custom_icons in _icon_collection.values():
-        bpy.utils.previews.remove(custom_icons)
-    _icon_collection.clear()
-
-def get_icon_switch():
-    """Return show_icons setting from addon preferences"""
-
-    addon = bpy.context.user_preferences.addons.get(addon_name)
-
-    if addon and hasattr(addon, "preferences"):
-        return addon.preferences.show_icons
-
-def icon(display_icon):
-    '''returns empty dict if show_icons is False, else the icon passed'''
-    kws = {}
-    if get_icon_switch():
-        if display_icon.startswith('SV_'):
-            kws = {'icon_value': custom_icon(display_icon)}
-        elif display_icon != 'OUTLINER_OB_EMPTY':
-            kws = {'icon': display_icon}
-    return kws
-
-
-def node_icon(node_ref):
-    '''returns empty dict if show_icons is False, else the icon passed'''
-    if not get_icon_switch():
-        return {}
-    else:
-        if hasattr(node_ref, 'sv_icon'):
-            iconID = custom_icon(node_ref.sv_icon)
-            return {'icon_value': iconID} if iconID else {}
-        elif hasattr(node_ref, 'bl_icon') and node_ref.bl_icon != 'OUTLINER_OB_EMPTY':
-            iconID = node_ref.bl_icon
-            return {'icon': iconID} if iconID else {}
-        else:
-            return {}
+def removeCustomIcons():
+    logDebug("unloadIcons called")
+    global custom_icons
+    bpy.utils.previews.remove(custom_icons)
 
 def register():
-    load_custom_icons()
-
+    logDebug("Registering SV custom icons")
+    loadCustomIcons()
 
 def unregister():
-    remove_custom_icons()
+    logDebug("Unregistering SV custom icons")
+    removeCustomIcons()
 
 if __name__ == '__main__':
     register()
+
