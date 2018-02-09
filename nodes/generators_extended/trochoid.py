32ea9526a66a7f8a6244e845b76eb7b4a1eefd4d
DolphinDream <marius.giurgi@gmail.com>
2018-02-08 23:38:37 -0500

    Various updates...optimising, renaming, clenaup, presets

--- nodes/generators_extended/trochoid.py
+++ nodes/generators_extended/trochoid.py
@@ -22,9 +22,24 @@ from bpy.props import BoolProperty, IntProperty, FloatProperty, EnumProperty
 from sverchok.node_tree import SverchCustomTreeNode
 from sverchok.data_structure import (match_long_repeat, updateNode)
 
-from math import sin, cos, pi, sqrt
+from math import sin, cos, pi
 
 typeItems = [("HYPO", "Hypo", ""), ("LINE", "Line", ""), ("EPI", "Epi", "")]
+
+# name : [ type, r1, r2, height, phase1, phase2, turns, resolution ]
+trochoidPresets = {
+    " ":            ["", 0.0, 0.0, 0.0, 0, 0, 0.0, 0.0, 1.0],
+    "CYCLOID":      ["LINE", 1.0, 1.0, 1.0, 0.0, 0.0, 2.0, 200, 1.0],
+    "CARDIOID":     ["EPI", 1.0, 1.0, 1.0, 0.0, 0.0, 1.0, 200, 1.0],
+    "NEPHROID":     ["EPI", 2.0, 1.0, 1.0, 0.0, 0.0, 1.0, 200, 1.0],
+    "RANUNCULOID":  ["EPI", 5.0, 1.0, 1.0, 0.0, 0.0, 1.0, 200, 1.0],
+    "DELTOID":      ["HYPO", 3.0, 1.0, 1.0, 0.0, 0.0, 1.0, 200, 1.0],
+    "ASTROID":      ["HYPO", 4.0, 1.0, 1.0, 0.0, 0.0, 1.0, 200, 1.0],
+    # and some interesting ones
+    "E6":           ["EPI", 6.0, 1.0, 5.0, 0.0, 0.0, 1.0, 300, 0.5],
+    "H5":           ["HYPO", 4.0, 1.0, 1.0, 0.0, 0.0, 1.0, 200, 1.0],
+
+}
 
 
 class SvTrochoidNode(bpy.types.Node, SverchCustomTreeNode):
@@ -33,33 +48,61 @@ class SvTrochoidNode(bpy.types.Node, SverchCustomTreeNode):
     bl_label = 'Trochoid'
     bl_icon = 'OUTLINER_OB_EMPTY'
 
+    def update_presets(self, context):
+        self.updating = True
+
+        if self.presets == " ":
+            self.updating = False
+            return
+
+        tt, r1, r2, h, p1, p2, T, N, S = trochoidPresets[self.presets]
+        self.tType = tt
+        self.radius1 = r1
+        self.radius2 = r2
+        self.height = h
+        self.phase1 = p1
+        self.phase2 = p2
+        self.turns = T
+        self.resolution = N
+        self.scale = S
+        self.closed = False if self.tType == "LINE" else True
+
+        self.updating = False
+        updateNode(self, context)
+
+    presetItems = [(k, k.title(), "", "", i) for i, (k, v) in enumerate(sorted(trochoidPresets.items()))]
+
+    presets = EnumProperty(
+        name="Presets", items=presetItems,
+        update=update_presets)
+
     tType = EnumProperty(
         name="Type", items=typeItems,
         description="Type of trochoid: Hypo, Line & Epi",
         default="EPI", update=updateNode)
 
     radius1 = FloatProperty(
-        name='Radius1', description='Radius1',
+        name='Radius1', description='Radius of the static circle',
         default=6.0, min=0.0, update=updateNode)
 
     radius2 = FloatProperty(
-        name='Radius2', description='Radius2',
+        name='Radius2', description='Radius of the rolling circle',
         default=1.0, min=0.0, update=updateNode)
 
     height = FloatProperty(
-        name='Height', description='Height',
+        name='Height', description='Distance from the center of the rolling circle to the moving point',
         default=5.0, min=0.0, update=updateNode)
 
-    offset1 = FloatProperty(
-        name='Offset1', description='Offset1',
-        default=0.0, min=0.0, max=1.0, update=updateNode)
+    phase1 = FloatProperty(
+        name='Phase1', description='Starting angle for the static circle (in radians)',
+        default=0.0, update=updateNode)
 
-    offset2 = FloatProperty(
-        name='Offset2', description='Offset2',
-        default=0.0, min=0.0, max=1.0, update=updateNode)
+    phase2 = FloatProperty(
+        name='Phase2', description='Starting angle for the rolling circle (in radians)',
+        default=0.0, update=updateNode)
 
-    time = FloatProperty(
-        name='Time', description='Time',
+    turns = FloatProperty(
+        name='Turns', description='Number of turns around the static circle',
         default=1.0, min=0.0, update=updateNode)
 
     closed = BoolProperty(
@@ -70,24 +113,32 @@ class SvTrochoidNode(bpy.types.Node, SverchCustomTreeNode):
         name='Swap', description='Swap',
         default=False, update=updateNode)
 
-    num_verts = IntProperty(
-        name='Num Verts', description='Number of vertices',
-        default=200, min=3, update=updateNode)
+    resolution = IntProperty(
+        name='Turn Resolution', description='Number of vertices per turn',
+        default=100, min=3, update=updateNode)
+
+    scale = FloatProperty(
+        name='Scale', description='Scale radii',
+        default=1.0, min=0.0, update=updateNode)
+
+    updating = BoolProperty(default=False)  # used for disabling update callback
 
     def sv_init(self, context):
         self.width = 150
         self.inputs.new('StringsSocket', "R1", "R1").prop_name = "radius1"
         self.inputs.new('StringsSocket', "R2",  "R2").prop_name = "radius2"
         self.inputs.new('StringsSocket', "H", "H").prop_name = "height"
-        self.inputs.new('StringsSocket', "O1", "O1").prop_name = "offset1"
-        self.inputs.new('StringsSocket', "O2", "O2").prop_name = "offset2"
-        self.inputs.new('StringsSocket', "T", "T").prop_name = "time"
-        self.inputs.new('StringsSocket', "N", "N").prop_name = "num_verts"
+        self.inputs.new('StringsSocket', "P1", "P1").prop_name = "phase1"
+        self.inputs.new('StringsSocket', "P2", "P2").prop_name = "phase2"
+        self.inputs.new('StringsSocket', "T", "T").prop_name = "turns"
+        self.inputs.new('StringsSocket', "N", "N").prop_name = "resolution"
+        self.inputs.new('StringsSocket', "S", "S").prop_name = "scale"
 
         self.outputs.new('VerticesSocket', "Verts", "Verts")
         self.outputs.new('StringsSocket', "Edges", "Edges")
 
     def draw_buttons(self, context, layout):
+        layout.prop(self, 'presets')
         col = layout.column(align=True)
         row = col.row(align=True)
         row.prop(self, "tType", expand=True)
@@ -95,29 +146,29 @@ class SvTrochoidNode(bpy.types.Node, SverchCustomTreeNode):
         row.prop(self, "closed", toggle=True)
         row.prop(self, "swap", toggle=True)
 
-    def make_trochoid(self, R1, R2, H, O1, O2, T, N):
+    def make_trochoid(self, R1, R2, H, P1, P2, T, N, S):
         verts = []
         edges = []
 
         a, b = [R2, R1] if self.swap else [R1, R2]
 
-        # cache here for performance
-        o1 = 2 * pi * O1
-        o2 = 2 * pi * O2
-        R = a + b  # outer radius
-        r = a - b  # inner radius
-        Rb = R / b  # outer "gear ratio"
-        rb = r / b  # inner "gear ratio"
+        a = a * S
+        b = b * S
+        h = H * S
 
         if self.tType == "EPI":
-            fx = lambda t: R * cos(t + o1) - H * cos(Rb * t + o2)
-            fy = lambda t: R * sin(t + o1) - H * sin(Rb * t + o2)
+            R = a + b  # outer radius
+            Rb = R / b  # outer "gear ratio"
+            fx = lambda t: R * cos(t + P1) - h * cos(Rb * t + P2)
+            fy = lambda t: R * sin(t + P1) - h * sin(Rb * t + P2)
         elif self.tType == "HYPO":
-            fx = lambda t: r * cos(t + o1) + H * cos(rb * t + o2)
-            fy = lambda t: r * sin(t + o1) - H * sin(rb * t + o2)
+            r = a - b  # inner radius
+            rb = r / b  # inner "gear ratio"
+            fx = lambda t: r * cos(t + P1) + h * cos(rb * t + P2)
+            fy = lambda t: r * sin(t + P1) - h * sin(rb * t + P2)
         else:  # LINE
-            fx = lambda t: a * t - H * sin(t)
-            fy = lambda t: a - H * cos(t)
+            fx = lambda t: a * t - H * sin(t + P1)
+            fy = lambda t: a - H * cos(t + P1)
 
         dT = 2 * pi * T / N
 
@@ -143,26 +194,26 @@ class SvTrochoidNode(bpy.types.Node, SverchCustomTreeNode):
         input_R1 = inputs["R1"].sv_get()[0]
         input_R2 = inputs["R2"].sv_get()[0]
         input_H = inputs["H"].sv_get()[0]
-        input_O1 = inputs["O1"].sv_get()[0]
-        input_O2 = inputs["O2"].sv_get()[0]
+        input_P1 = inputs["P1"].sv_get()[0]
+        input_P2 = inputs["P2"].sv_get()[0]
         input_T = inputs["T"].sv_get()[0]
         input_N = inputs["N"].sv_get()[0]
+        input_S = inputs["S"].sv_get()[0]
 
         # sanitize the inputs
         input_R1 = list(map(lambda x: max(0.0, x), input_R1))
         input_R2 = list(map(lambda x: max(0.0, x), input_R2))
         input_H = list(map(lambda x: max(0.0, x), input_H))
-        input_O1 = list(map(lambda x: max(0.0, min(1.0, x)), input_O1))
-        input_O2 = list(map(lambda x: max(0.0, min(1.0, x)), input_O2))
         input_T = list(map(lambda x: max(0.0, x), input_T))
         input_N = list(map(lambda x: max(3, int(x)), input_N))
+        input_S = list(map(lambda x: max(0.0, x), input_S))
 
-        parameters = match_long_repeat([input_R1, input_R2, input_H, input_O1, input_O2, input_T, input_N])
+        parameters = match_long_repeat([input_R1, input_R2, input_H, input_P1, input_P2, input_T, input_N, input_S])
 
         vertList = []
         edgeList = []
-        for R1, R2, H, O1, O2, T, N in zip(*parameters):
-            verts, edges = self.make_trochoid(R1, R2, H, O1, O2, T, N)
+        for R1, R2, H, P1, P2, T, N, S in zip(*parameters):
+            verts, edges = self.make_trochoid(R1, R2, H, P1, P2, T, 1 + int(N * T), S)
             vertList.append(verts)
             edgeList.append(edges)
 
